8c05c67a21e2b64a61f443e72140e46f9025c7ae
Dakota States <dakota.states@gmail.com>
2020-02-20 22:39:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,18 @@
 class Dog 
-  @@all = []
-  attr_accessor
+  attr_accessor :name
   
+  @@all = []
+ @name = name
   def initialize
     
+    @@all << self
   end
   
   def self.all
+    @@all
+  end
+  
+  def self.print_all
     @@all
   end
 end
